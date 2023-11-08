45f86f921f281b329c488b110810e8410553beeb
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-08 21:29:21 +0000

    Creates 3 methods (read database credentials, create engine, list database table names)

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,29 @@
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+
+
 class DatabaseConnector:
     def __init__(self):
+        self.temp = None    
+    
+    
+    def read_db_creds(self):
+        with open('db_creds.yaml' , 'r') as file:
+            load_as_dict = yaml.safe_load(file) 
+            return load_as_dict
+
+    def init_db_engine(self):
+        creds = self.read_db_creds()         
+        engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
+        return engine
+
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        table_list = inspector.get_table_names()
+        return table_list
+
+    
+test = DatabaseConnector()
+print(test.list_db_tables())
