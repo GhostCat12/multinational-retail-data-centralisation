6d93da1f92437149582b0161ae17e0fa1ea6ee9f
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-10 18:50:44 +0000

    imports DatabaseConnecter class, to run the upload_to_db method in run_data.py

--- run_data.py
+++ run_data.py
@@ -1,4 +1,9 @@
 from data_cleaning import DataCleaning
+from database_utils import DatabaseConnector
 
 clean = DataCleaning()
-clean.clean_user_data()
+cleaned = clean.clean_user_data()
+
+
+connect = DatabaseConnector()
+connect.upload_to_db(pandas_df=cleaned, table_name ='dim_users')
