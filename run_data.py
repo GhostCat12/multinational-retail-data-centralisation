81cf72d366a8c1c2bcb5133730801d0fe03a3829
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-12 16:34:50 +0000

    imports DataExtractor class and runs retrieve_pdf_data method

--- run_data.py
+++ run_data.py
@@ -1,9 +1,18 @@
 from data_cleaning import DataCleaning
 from database_utils import DatabaseConnector
+from data_extraction import DataExtractor
+
 
 clean = DataCleaning()
-cleaned = clean.clean_user_data()
+#cleaned = clean.clean_user_data()
 
 
 connect = DatabaseConnector()
-connect.upload_to_db(pandas_df=cleaned, table_name ='dim_users')
+#connect.upload_to_db(pandas_df=cleaned, table_name ='dim_users')
+
+link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+
+extract = DataExtractor()
+extract.retrieve_pdf_data(link)
+
+
