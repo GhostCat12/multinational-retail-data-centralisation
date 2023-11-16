ec02fe558b6494f7cc724d05ac00142a44bb61a4
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-16 21:45:29 +0000

    updates run_data.py to run convert_product_weights() method from DataCleaning class in data_cleaning.py

--- run_data.py
+++ run_data.py
@@ -4,17 +4,15 @@ from data_extraction import DataExtractor
 
 
 clean = DataCleaning()
-
-
 #cleaned_user_data = clean.clean_user_data()
 #cleaned_card_details = clean.clean_card_data()
-cleaned_store_data = clean.clean_store_data()
-
+#cleaned_store_data = clean.clean_store_data()
+cleaned_products_data = clean.convert_product_weights()
 
 connect = DatabaseConnector()
 #connect.upload_to_db(pandas_df=cleaned_user_data, table_name ='dim_users')
 #connect.upload_to_db(pandas_df=cleaned_card_details, table_name ='dim_cards_details')
-connect.upload_to_db(pandas_df=cleaned_store_data, table_name ='dim_store_details')
+#connect.upload_to_db(pandas_df=cleaned_store_data, table_name ='dim_store_details')
 
 #link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
 
@@ -27,6 +25,11 @@ extract = DataExtractor()
 
 #store_url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
 #extract.retrieve_stores_data(store_url, 'api_key.yaml')
+
+#address = 's3://data-handling-public/products.csv'
+#extract.extract_from_s3(address)
+
+
 
 
 
