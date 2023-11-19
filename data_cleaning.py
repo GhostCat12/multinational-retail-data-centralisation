cc051388d0faf5162b921c50bbc7ec6f9aa750c3
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-19 21:31:36 +0000

    updates code in data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -292,7 +292,6 @@ class DataCleaning:
                               ALTER TABLE dim_store_details ALTER COLUMN staff_numbers TYPE SMALLINT;
                               ALTER TABLE dim_store_details ALTER COLUMN opening_date TYPE DATE;
                               ALTER TABLE dim_store_details ALTER COLUMN store_type TYPE VARCHAR(255);
-                              ALTER TABLE dim_store_details ALTER COLUMN store_type VARCHAR(255) NULL;
                               ALTER TABLE dim_store_details ALTER COLUMN latitude TYPE FLOAT;
                               ALTER TABLE dim_store_details ALTER COLUMN country_code TYPE VARCHAR(2);
                               ALTER TABLE dim_store_details ALTER COLUMN continent TYPE VARCHAR(255)
