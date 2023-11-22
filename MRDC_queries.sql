f6cc6b0e462efe27f79fcd02dd708968e33d4f8f
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-22 22:21:17 +0000

    updates queries in MRDC_queries.sql

--- MRDC_queries.sql
+++ MRDC_queries.sql
@@ -65,24 +65,131 @@ LIMIT 6
 "03"	645462.9999999898 */
 
 
+/* The company is looking to increase its online sales. They want to know how many sales are happening online vs offline. 
+Calculate how many products were sold and the amount of sales made for online and offline purchases. */
+
+SELECT 
+	CASE 
+		WHEN store_code = 'WEB-1388012W' THEN 'Web'
+		ELSE 'Offline'
+	END AS "location",
+	SUM(product_quantity) AS product_quantity_count,
+	COUNT(product_quantity*product_price_£) AS number_of_sales
+FROM orders_table
+FULL OUTER JOIN dim_products
+	ON orders_table.product_code = dim_products.product_code
+GROUP BY "store_code" = 'WEB-1388012W'
+
+/* Output 
+Location   product_quantity_count 	number_of_sales
+"Offline"  374047	                93166
+"Web"	   107739	                26957
+*/
 
 
+/* The sales team wants to know which of the different store types is generated the most revenue so they know where to focus.
+Find out the total and percentage of sales coming from each of the different store types. */
+
+with t(store_type,total_sales)
+as
+(
+	SELECT 
+		store_type,
+		SUM(product_quantity * product_price_£) AS total_sales
+	FROM orders_table
+		FULL OUTER JOIN dim_products
+		ON orders_table.product_code = dim_products.product_code
+		FULL OUTER JOIN dim_store_details
+		ON dim_store_details.store_code = orders_table.store_code
+	GROUP BY store_type
+)
+SELECT 
+	store_type,
+	total_sales,
+	total_sales * 100 / (SELECT SUM(total_sales) FROM t) as percentage
+FROM t	
+
+/*
+store_type   	   total_sales  	  percentage
+"Super Store"	1224293.6499999538	15.853933630352115
+"Web Portal"	1726547.0499999092	22.357840653980215
+"Local"	3440896.5200001546	44.55772931354668
+"Outlet"	631804.8099999899	8.18152697686331
+"Mall Kiosk"	698791.6099999876	9.048969425257674
+*/
 
 
+/* The company stakeholders want assurances that the company has been doing well recently.
+Find which months in which years have had the most sales historically. */
+
+SELECT 
+	SUM(product_price_£ * product_quantity) AS total_sales,
+	"year",
+	"month"
+FROM orders_table
+FULL OUTER JOIN dim_date_times
+	ON orders_table.date_uuid = dim_date_times.date_uuid
+FULL OUTER JOIN dim_products
+ ON orders_table.product_code = dim_products.product_code
+GROUP BY "year", "month"
+ORDER BY total_sales DESC
+
+/* 
+total_sales              year   month
+27936.769999999993	    "1994"	"03"
+27356.14	            "2019"	"01"
+27091.66999999999	    "2009"	"08"
+26679.979999999992	    "1997"	"11"
+26310.969999999998	    "2018"	"12"
+26277.719999999994	    "2019"	"08"
+26236.670000000006	    "2017"	"09"
+25798.119999999995	    "2010"	"05"
+25648.28999999999	    "1996"	"08"
+25614.539999999983	    "2000"	"01"
+*/
+
+/*The operations team would like to know the overall staff numbers in each location around the world. Perform a query to determine the staff numbers in each of the countries the company sells in.
+The query should return the values:*/
 
 
+SELECT
+SUM(staff_numbers) AS total_staff_numbers,
+dim_store_details.country_code
+FROM orders_table
+FULL OUTER JOIN dim_store_details
+	ON orders_table.store_code = dim_store_details.store_Code                          ### 	QUESTION THIS 	###
+GROUP BY dim_store_details.country_code
+
+/*
+total_staff_numbers  Country
+298156	                "US"
+11506604	            "GB"
+1307021	                "DE"
+*/
+
+/*The sales team is looking to expand their territory in Germany. Determine which type of store is generating the most sales in Germany*/
+
+SELECT 
+SUM(product_price_£ * product_quantity) AS total_sales,
+store_type,
+country_code
+FROM orders_table
+FULL OUTER JOIN dim_store_details
+	ON orders_table.store_code = dim_store_details.store_code
+FULL OUTER JOIN dim_products
+	ON orders_table.product_Code = dim_products.product_Code
+WHERE "country_code" = 'DE'
+GROUP by store_type, country_code
+
+/*
+total_sales         store_type      country_code
+384625.02999999834	"Super Store"	"DE"
+247634.20000000042	"Mall Kiosk"	"DE"
+1109909.5899999617	"Local"	        "DE"
+198373.57000000039	"Outlet"	    "DE"
+*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+/*Sales would like the get an accurate metric for how quickly the company is making sales.
+Determine the average time taken between each sale grouped by year*/
 
