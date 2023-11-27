a7d8b97a8dee264aea8ec0bd5f752184315c0bae
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-11-27 01:05:59 +0000

    creates links.py file and adds urls

---
+++ links.py
@@ -0,0 +1,12 @@
+card_details = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
+
+number_of_stores = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+
+stores_data = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
+
+products_data = 's3://data-handling-public/products.csv'
+
+date_details = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
+
+
+
